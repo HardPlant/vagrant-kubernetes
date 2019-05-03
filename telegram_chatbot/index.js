3fa1849e5f36b7961af867a629ca755bbec01543
HardPlant <abc7988se@naver.com>
2019-05-03 15:54:10 +0900

    simple program

--- telegram_chatbot/index.js
+++ telegram_chatbot/index.js
@@ -1,3 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-console.log("Hello");
+var config = fs.readFileSync('config.yaml');
+token = yaml.load(config)["token"];
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, "I'll have the tuna?");
+});
