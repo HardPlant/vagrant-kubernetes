f274e87729cee65b2f7e478e43524d89247408da
HardPlant <abc7988se@naver.com>
2019-05-03 17:11:39 +0900

    versioning deploy

--- telegram_chatbot/deploy.bat
+++ telegram_chatbot/deploy.bat
@@ -2,5 +2,5 @@ SET IMAGE=hp_build_bot
 SET VERSION=1.0.0.3
 SET DEPLOY_FILE=deployments.yml
 powershell -Command "(gc %DEPLOY_FILE%) -replace 'image: %IMAGE%', 'image: %IMAGE%:%VERSION% #' | Out-File %DEPLOY_FILE%"
-REM docker build -t %IMAGE%:%VERSION% .
-REM kubectl apply -f deployments.yml
+docker build -t %IMAGE%:%VERSION% .
+kubectl apply -f deployments.yml
